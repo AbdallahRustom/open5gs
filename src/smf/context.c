8b7f3be3772f192de17ed33d77e0e51850c9bab5
Abdallah <abdallah.rustom.ar@gmail.com>
2023-09-13 15:43:14 +0000

    update2

--- src/smf/context.c
+++ src/smf/context.c
@@ -2343,7 +2343,19 @@ smf_bearer_t *smf_bearer_add(smf_sess_t *sess)
     smf_pf_identifier_pool_init(bearer);
 
     ogs_list_init(&bearer->pf_list);
+    
+    /* If usage logging enabled create a new URR */
+    if (ogs_pfcp_self()->usageLoggerState.enabled) {
+        urr = ogs_pfcp_urr_add(&sess->pfcp);
+        ogs_assert(urr);
 
+        urr->meas_method = OGS_PFCP_MEASUREMENT_METHOD_DURATION;
+        urr->rep_triggers.time_threshold = 1;
+        urr->time_threshold = ogs_pfcp_self()->usageLoggerState.reporting_period_sec;
+        /* Enable Immediate Start Time Metering */
+        urr->meas_info.istm = 1;
+    }
+ 
     /* PDR */
     dl_pdr = ogs_pfcp_pdr_add(&sess->pfcp);
     ogs_assert(dl_pdr);
