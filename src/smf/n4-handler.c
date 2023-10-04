e51c558db7f736467b3e23bee4098de171293fb6
AbdallahRustom <abdallah.rustom.ar@gmail.com>
2023-10-04 23:02:39 +0000

    update40

--- src/smf/n4-handler.c
+++ src/smf/n4-handler.c
@@ -173,6 +173,11 @@ uint8_t smf_5gc_n4_handle_session_establishment_response(
     ogs_debug("Session Establishment Response [5gc]");
 
     ogs_pfcp_xact_commit(xact);
+
+    if (ogs_pfcp_self()->usageLoggerState.enabled) {
+        ogs_error("usageloggerstate is enabled");
+        log_start_usage_reports(sess);
+    }
     
     if (rsp->up_f_seid.presence == 0) {
         ogs_error("No UP F-SEID");
@@ -206,11 +211,6 @@ uint8_t smf_5gc_n4_handle_session_establishment_response(
     ogs_list_for_each(&sess->pfcp.pdr_list, pdr) {
         far = pdr->far;
         ogs_assert(far);
-
-        if (ogs_pfcp_self()->usageLoggerState.enabled) {
-        ogs_error("usageloggerstate is enabled");
-        log_start_usage_reports(sess);
-        }
 
         if (pdr->src_if == OGS_PFCP_INTERFACE_ACCESS) {
             if (far->dst_if == OGS_PFCP_INTERFACE_CP_FUNCTION)
@@ -1494,7 +1494,7 @@ static UsageLoggerData build_usage_logger_data(smf_sess_t *sess, char const* eve
     strcpy(usageLoggerData.charging_id, "<charging_id placeholder>");
     strncpy(usageLoggerData.msisdn_bcd, smf_ue->msisdn_bcd, MSISDN_BCD_STR_MAX_LEN);
     strncpy(usageLoggerData.imeisv_bcd, smf_ue->imeisv_bcd, IMEISV_BCD_STR_MAX_LEN);
-    if (!hex_array_to_string(smf_ue->timezone_raw, smf_ue->timezone_raw_len, usageLoggerData.timezone_raw, TIMEZONE_RAW_STR_MAX_LEN)) {
+    if (!hex_array_to_string(sess->gtp.ue_timezone.data, sess->gtp.ue_timezone.len, usageLoggerData.timezone_raw, TIMEZONE_RAW_STR_MAX_LEN)) {
         ogs_error("Failed to convert raw timezone bytes to timezone hex string!");
     }
     usageLoggerData.plmn = ogs_plmn_id_hexdump(&sess->e_tai.plmn_id);
@@ -1502,7 +1502,6 @@ static UsageLoggerData build_usage_logger_data(smf_sess_t *sess, char const* eve
     usageLoggerData.eci = sess->e_cgi.cell_id;
 
     const ogs_ip_t* ip_ptr = (const ogs_ip_t*)sess->ipv4->addr;
-
     if (!ogs_ip_to_string(ip_ptr, usageLoggerData.ue_ip, IP_STR_MAX_LEN)) {
        ogs_error("Failed to convert raw UE IP bytes");
     }
