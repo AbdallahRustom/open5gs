153f893401f0bc8a846ae75f2bb170dfec67ff1b
Harald Welte <laforge@osmocom.org>
2024-06-04 22:35:14 +0200

    add license header to source files where it's missing
    Change-Id: I1e4489335f103266c5fc1e4e87422f19d4f7ae1a

--- src/aaa_diameter_swm.erl
+++ src/aaa_diameter_swm.erl
@@ -1,6 +1,40 @@
 % AAA Server implementation of SWm Diameter interface, TS 29.273 section 7
 % This interface is so far implemented through internal erlang messages against
 % the internal ePDG.
+
+% (C) 2023 by sysmocom - s.f.m.c. GmbH <info@sysmocom.de>
+% Author: Pau Espin Pedrol <pespin@sysmocom.de>
+%
+% All Rights Reserved
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU Affero General Public License as
+% published by the Free Software Foundation; either version 3 of the
+% License, or (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU Affero General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%
+% Additional Permission under GNU AGPL version 3 section 7:
+%
+% If you modify this Program, or any covered work, by linking or
+% combining it with runtime libraries of Erlang/OTP as released by
+% Ericsson on http://www.erlang.org (or a modified version of these
+% libraries), containing parts covered by the terms of the Erlang Public
+% License (http://www.erlang.org/EPLICENSE), the licensors of this
+% Program grant you additional permission to convey the resulting work
+% without the need to license the runtime libraries of Erlang/OTP under
+% the GNU Affero General Public License. Corresponding Source for a
+% non-source form of such a combination shall include the source code
+% for the parts of the runtime libraries of Erlang/OTP used as well as
+% that of the covered work.
+
+
 -module(aaa_diameter_swm).
 -behaviour(gen_server).
 
