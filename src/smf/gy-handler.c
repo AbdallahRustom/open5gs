e6a7b61c6a10cc4252fa6199a2c8a3d899a5d0aa
abdallah <abdallah.r.waly@orange.com>
2025-05-02 15:18:10 +0300

    update handel delete session request for support of gy

--- src/smf/gy-handler.c
+++ src/smf/gy-handler.c
@@ -134,14 +134,14 @@ uint32_t smf_gy_handle_cca_initial_request(
     bearer = smf_default_bearer_in_sess(sess);
     ogs_assert(bearer);
 
-    if (!bearer->urr)
+    if (!bearer->urr){
         bearer->urr = ogs_pfcp_urr_add(&sess->pfcp);
-    ogs_assert(bearer->urr);
-    bearer->urr->meas_method = OGS_PFCP_MEASUREMENT_METHOD_VOLUME;
-    bearer->urr->rep_triggers.volume_threshold = 1;
-    bearer->urr->vol_threshold.tovol = 1;
-    bearer->urr->vol_threshold.total_volume = 1024*1024*100;
-    
+        ogs_assert(bearer->urr);
+        bearer->urr->meas_method = OGS_PFCP_MEASUREMENT_METHOD_VOLUME;
+        bearer->urr->rep_triggers.volume_threshold = 1;
+        bearer->urr->vol_threshold.tovol = 1;
+        bearer->urr->vol_threshold.total_volume = 1024*1024*100;
+    }
     /************srag&abdallah*********/
     if(smf_self()->use_radius == true )
     {
